98ab0c86bc62c03dab89acdaf8ac0f175cfb8ae0
wilsonpeng20070926-beep <wilsonpeng20070926@gmail.com>
2026-07-01 14:34:06 +0800

    Prepare Draft Coach public release

---
+++ test/fixtures/championFixture.ts
@@ -0,0 +1,128 @@
+import { createChampionCatalogFromDataDragonData } from "../../src/main/catalog/championCatalog";
+
+export function createFixtureCatalog() {
+  return createChampionCatalogFromDataDragonData("15.10.1", {
+    data: {
+      Aatrox: {
+        key: "266",
+        id: "Aatrox",
+        name: "Aatrox",
+        tags: ["Fighter", "Tank"],
+      },
+      Ahri: {
+        key: "103",
+        id: "Ahri",
+        name: "Ahri",
+        tags: ["Mage", "Assassin"],
+      },
+      Darius: {
+        key: "122",
+        id: "Darius",
+        name: "Darius",
+        tags: ["Fighter", "Tank"],
+      },
+      Jax: {
+        key: "24",
+        id: "Jax",
+        name: "Jax",
+        tags: ["Fighter", "Assassin"],
+      },
+      Jinx: {
+        key: "222",
+        id: "Jinx",
+        name: "Jinx",
+        tags: ["Marksman"],
+      },
+      MonkeyKing: {
+        key: "62",
+        id: "MonkeyKing",
+        name: "Wukong",
+        tags: ["Fighter", "Tank"],
+      },
+      Nocturne: {
+        key: "56",
+        id: "Nocturne",
+        name: "Nocturne",
+        tags: ["Assassin", "Fighter"],
+      },
+      JarvanIV: {
+        key: "59",
+        id: "JarvanIV",
+        name: "Jarvan IV",
+        tags: ["Tank", "Fighter"],
+      },
+      Brand: {
+        key: "63",
+        id: "Brand",
+        name: "Brand",
+        tags: ["Mage"],
+      },
+      Gragas: {
+        key: "79",
+        id: "Gragas",
+        name: "Gragas",
+        tags: ["Fighter", "Mage"],
+      },
+      Orianna: {
+        key: "61",
+        id: "Orianna",
+        name: "Orianna",
+        tags: ["Mage", "Support"],
+      },
+      Fizz: {
+        key: "105",
+        id: "Fizz",
+        name: "Fizz",
+        tags: ["Assassin", "Fighter"],
+      },
+      Lissandra: {
+        key: "127",
+        id: "Lissandra",
+        name: "Lissandra",
+        tags: ["Mage"],
+      },
+      Riven: {
+        key: "92",
+        id: "Riven",
+        name: "Riven",
+        tags: ["Fighter", "Assassin"],
+      },
+      Sett: {
+        key: "875",
+        id: "Sett",
+        name: "Sett",
+        tags: ["Fighter", "Tank"],
+      },
+      Thresh: {
+        key: "412",
+        id: "Thresh",
+        name: "Thresh",
+        tags: ["Support", "Fighter"],
+      },
+      Yasuo: {
+        key: "157",
+        id: "Yasuo",
+        name: "Yasuo",
+        tags: ["Fighter", "Assassin"],
+      },
+      Vladimir: {
+        key: "8",
+        id: "Vladimir",
+        name: "Vladimir",
+        tags: ["Mage"],
+      },
+      Xerath: {
+        key: "101",
+        id: "Xerath",
+        name: "Xerath",
+        tags: ["Mage"],
+      },
+      Zaahen: {
+        key: "904",
+        id: "Zaahen",
+        name: "Zaahen",
+        tags: ["Fighter", "Tank"],
+      },
+    },
+  });
+}
